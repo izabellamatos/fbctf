860d4a7a8531282441d4db0b76c843278464102d
izabellamatos <bell.silvamatos@gmail.com>
2017-10-03 18:56:38 -0300

    Adicionado tradução ao Game Controls

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -1195,7 +1195,7 @@ class AdminController extends Controller {
     return
       <div>
         <header class="admin-page-header">
-          <h3>Game Controls</h3>
+          <h3>{tr('Game Controls')}</h3>
           <span class="admin-section--status">
             {tr('status_')}<span class="highlighted">{tr('OK')}</span>
           </span>
